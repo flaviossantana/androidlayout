1dd5c0011d77930ee3f8b118432859fa11a62518
flavio.santana <9090flavio@gmail.com>
2018-03-09 23:52:53 -0300

    Projeto Lombok: Escrevendo menos código em Java: https://medium.com/collabcode/projeto-lombok-escrevendo-menos-c%C3%B3digo-em-java-8fc87b379209

--- app/src/main/java/br/com/alura/androilayout/model/Pacote.java
+++ app/src/main/java/br/com/alura/androilayout/model/Pacote.java
@@ -3,57 +3,20 @@ package br.com.alura.androilayout.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-/**
- * Created by f1avi on 09/03/2018.
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pacote implements Serializable {
 
     private String local;
+    private String imagem;
     private int dias;
     private BigDecimal valor;
-    private String imagem;
 
-    public Pacote() {
-        super();
-    }
-
-    public Pacote(String local, String imagem, int dias, BigDecimal valor) {
-        this.local = local;
-        this.dias = dias;
-        this.valor = valor;
-        this.imagem = imagem;
-    }
-
-    public String getLocal() {
-        return local;
-    }
-
-    public void setLocal(String local) {
-        this.local = local;
-    }
-
-    public int getDias() {
-        return dias;
-    }
-
-    public void setDias(int dias) {
-        this.dias = dias;
-    }
-
-    public BigDecimal getValor() {
-        return valor;
-    }
-
-    public void setValor(BigDecimal valor) {
-        this.valor = valor;
-    }
-
-    public String getImagem() {
-        return imagem;
-    }
-
-    public void setImagem(String imagem) {
-        this.imagem = imagem;
-    }
 }
