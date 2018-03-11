b30e55a9bb43f728e97760fd0697eb99be742dd9
flavio.santana <9090flavio@gmail.com>
2018-03-11 19:56:45 -0300

    Refatoração de código.

--- app/src/main/java/br/com/alura/androilayout/PagamentoActivity.java
+++ app/src/main/java/br/com/alura/androilayout/PagamentoActivity.java
@@ -2,17 +2,40 @@ package br.com.alura.androilayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import br.com.alura.androilayout.model.Pacote;
 import br.com.alura.androilayout.util.MoedaUtil;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PagamentoActivity extends AppCompatActivity {
 
     @BindView(R.id.pagamento_valor_compra)
     TextView valorCompra;
+
+    @BindView(R.id.pagamento_nmumero_cartao)
+    EditText numeroCartao;
+
+    @BindView(R.id.pagamento_mes)
+    EditText mesCartao;
+
+    @BindView(R.id.pagamento_ano)
+    EditText anoCartao;
+
+    @BindView(R.id.pagamento_cvc)
+    EditText cvcCartao;
+
+    @BindView(R.id.pagamento_nome_cartao)
+    EditText nomeNoCartao;
+
+    @BindView(R.id.pagamento_finalizar_compra)
+    Button finalizarCompra;
+
+    private Pacote pacote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +44,16 @@ public class PagamentoActivity extends AppCompatActivity {
         setTitle("Pagamento do Pacote");
         ButterKnife.bind(this);
 
-        Pacote pacote = (Pacote) getIntent().getSerializableExtra("pacote");
+        pacote = (Pacote) getIntent().getSerializableExtra("pacote");
         valorCompra.setText(MoedaUtil.formatarValor(pacote.getValor()));
 
     }
+
+    @OnClick(R.id.pagamento_finalizar_compra)
+    public void onClickFinalizaraCompra(){
+
+
+
+    }
+
 }
